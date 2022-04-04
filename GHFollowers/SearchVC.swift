caf824d89c5186534737cc5e6da1e6d83988632f
Nicolas Rios <Nickrios33@yahoo.com>
2022-04-04 15:41:11 -0700

    set up tabbar programmatically

--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -1,14 +1,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        view.backgroundColor = .yellow
         
-        view.backgroundColor = .systemMint
     }
-
-
+    
 }
-
