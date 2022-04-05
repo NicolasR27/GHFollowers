58dc3a544085a0569d1cb0313dc63378ab4fbdac
Nicolas Rios <Nickrios33@yahoo.com>
2022-04-04 19:43:49 -0700

    implemented textfield  and call to action button

--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -12,6 +12,8 @@ class SearchVC: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         configureLogoImageView()
+        configureTextField()
+        configureCallToActionButton()
         
         
     }
@@ -32,6 +34,27 @@ class SearchVC: UIViewController {
             logoImageView.widthAnchor.constraint(equalToConstant: 200),
                                     
                                     
+        ])
+    }
+    
+    func configureTextField(){
+        view.addSubview(usernameTextField)
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 40),
+            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -50),
+            usernameTextField.heightAnchor.constraint(equalToConstant: 50)
+            
+        ])
+    }
+    
+   func configureCallToActionButton(){
+      view.addSubview(callToActionButton)
+        NSLayoutConstraint.activate([
+            callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,constant: -50),
+           callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+          callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+           callToActionButton.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
 }
