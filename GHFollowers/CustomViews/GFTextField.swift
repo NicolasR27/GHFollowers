2610e15295b18f000d20fdddb0f3635b50adb834
Nicolas Rios <Nickrios33@yahoo.com>
2022-04-13 21:59:24 -0700

    setup collectionview

--- GHFollowers/CustomViews/GFTextField.swift
+++ GHFollowers/CustomViews/GFTextField.swift
@@ -9,8 +9,8 @@ class GFTextField: UITextField {
     }
 
     
-    required init?(coder:NSCoder){
-        fatalError()
+    required init?(coder: NSCoder){
+        fatalError("init(coder:) has not been implemented")
     }
     
     private func configure() {
--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -4,31 +4,46 @@ import UIKit
 class FollowerListVC: UIViewController {
     
     var username: String!
-    
+    var collectionView: UICollectionView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewController()
+        configureCollectionView()
+        getFollowers()
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    
+    func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
-        
-        
+    }
+    
+    
+    func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    
+    func getFollowers() {
         NetworkManager.shared.getFollowers(for: username, page: 1) { result in
             
             switch result {
-            case.success(let followers):
+            case .success(let followers):
                 print(followers)
                 
-                case.failure(let error):
-                self.presentGFAlertOnMainThread(title: "Bad stuff happened", message: error.rawValue, buttonTitle: "ok")
-                
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: error.rawValue, buttonTitle: "Ok")
             }
-            
         }
-        
-        
     }
-
-override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-    navigationController?.setToolbarHidden(false, animated: true)
- }
 }
