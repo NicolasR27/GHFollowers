f54c3cf433c98e25f1343fbb149d94742c76370c
Nicolas Rios <Nickrios33@yahoo.com>
2022-04-14 21:30:47 -0700

    set up image caching

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -1,9 +1,11 @@
 
 import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
-    let baseURL = "https://api.github.com/users/"
+     private let baseURL = "https://api.github.com/users/"
+    let cache =  NSCache<NSString,UIImage>()
     
     public init() {}
     
