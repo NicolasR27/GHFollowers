1e0f37c02691942b4fd0cab4940a82818b0c88c8
Nicolas Rios <Nickrios33@yahoo.com>
2022-04-15 20:45:13 -0700

    dimiss loading view

--- GHFollowers/Extensions/UIViewController+Ext.swift
+++ GHFollowers/Extensions/UIViewController+Ext.swift
@@ -1,16 +1,47 @@
 
 import UIKit
 
+fileprivate var containerView: UIView!
+
 extension UIViewController {
     
-   func presentGFAlertOnMainThread(title: String,message: String, buttonTitle: String) {
+    
+    func presentGFAlertOnMainThread(title: String,message: String, buttonTitle: String) {
         DispatchQueue.main.async {
             let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
             alertVC.modalPresentationStyle = .overFullScreen
             alertVC.modalTransitionStyle = .crossDissolve
             self.present(alertVC,animated: true)
             
-           }
-           
+        }
+        
     }
+    
+    func showLoadingView() {
+        #warning("call dismiss")
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha  = 0
+        UIView.animate(withDuration: 0.25) {containerView.alpha = 0.8}
+        
+        let activityIndicator = UIActivityIndicatorView(style:.large)
+        containerView.addSubview(activityIndicator)
+        
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        activityIndicator.startAnimating()
+        
+    }
+    func dismissLoadingView(){
+        DispatchQueue.main.async{
+        containerView.removeFromSuperview()
+        containerView = nil
+    }
+}
 }
